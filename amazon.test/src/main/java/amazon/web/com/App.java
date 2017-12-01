14c1e71f96ebfe562c193d1dff104996b13c43b4
mjaved <mohammedmiandad@gmail.com>
2017-12-01 10:40:11 -0600

    added amazon.test file to spark repo

---
+++ amazon.test/src/main/java/amazon/web/com/App.java
@@ -0,0 +1,13 @@
+package amazon.web.com;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
